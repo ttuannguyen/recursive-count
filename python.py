b225d90db4b0709c0840f2cf795f2a6616bf968a
Tuan Nguyen <trentontuannguyen@gmail.com>
2023-07-21 17:13:44 -0400

    add practice example

--- python.py
+++ python.py
@@ -1,15 +1,28 @@
-count = 0
-while  count < 10:
-    # print(count)
-    count += 1
+# count = 0
+# while  count < 10:
+#     # print(count)
+#     count += 1
 
 # Challenge: convert the while loop to recursive method that counts
 
-def recursive(n = 0):
-    print(n)
-    count = 10
-    if n > count-2:
+def recursive(num = 0):
+    if num >= 10:
         return 
-    recursive(n + 1)
+    print(num)
+    recursive(num + 1)
 
 recursive()
+
+
+# Practice: 
+# count = 0
+# def recursive(count):
+#     # print(count) => 0, 1, 2, 3, 4
+#     if count > 3:
+#         return 'done'
+#         # when count = 4, it should return 'done'
+#     count += 1
+#     # without the return keyword we're getting 'none' as we pop off things from the call-stack bc the recursive() function itself does not return anything
+#     return recursive(count)
+    
+# print(recursive(count))
